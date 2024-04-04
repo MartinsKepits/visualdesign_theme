8c2eb49f87fdc62286388774718deeda85edd3c0
Martins Kepits <martinskepitslv@gmail.com>
2024-04-04 12:31:18 -0400

    Fix homepage hero on large screens

--- assets/js/sections/home-hero.js
+++ assets/js/sections/home-hero.js
@@ -1,38 +1,37 @@
 "use strict";
 
 $(document).ready(function () {
-    const slickSliderBreakpoint = window.screen.width > 1920;
-    let variableWidthVal = false;
+  const slickSliderBreakpoint = window.screen.width > 1920;
+  let variableWidthVal = false;
 
-    if (slickSliderBreakpoint) {
-        variableWidthVal = true;
-    }
+  if (slickSliderBreakpoint) {
+    variableWidthVal = true;
+  }
 
-    $(".block-hero").slick({
-        centerMode: true,
-        centerPadding: "60px",
-        slidesToShow: 1,
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        autoplay: true,
-        autoplaySpeed: 7000,
-        variableWidth: variableWidthVal,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    centerPadding: "30px",
-                },
-            },
-        ],
-    });
+  $(".block-hero").slick({
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 1,
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          centerPadding: "30px",
+        },
+      },
+    ],
+  });
 
-    if (slickSliderBreakpoint) {
-        $(".block-hero .slick-slide").width(1920);
-    }
+  if (slickSliderBreakpoint) {
+    $(".block-hero .slick-slide").width(1920);
+  }
 
-    $(".slide-img").click((e) => {
-        $(e.target).children(".slide-view-btn").toggleClass("active");
-    });
+  $(".slide-img").click((e) => {
+    $(e.target).children(".slide-view-btn").toggleClass("active");
+  });
 });
